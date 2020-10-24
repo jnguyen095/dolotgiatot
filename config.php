9685f615e4b6f9a43b2bf5594e378a2af9a7a310
Khang Nguyen <khang.nguyen@banvien.com>
2020-10-24 09:40:03 +0700

    First commit

---
+++ config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/dolotgiatot/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/dolotgiatot/');
+
+// DIR
+define('DIR_APPLICATION', 'D:/xampp/htdocs/dolotgiatot/catalog/');
+define('DIR_SYSTEM', 'D:/xampp/htdocs/dolotgiatot/system/');
+define('DIR_IMAGE', 'D:/xampp/htdocs/dolotgiatot/image/');
+define('DIR_STORAGE', 'D:/xampp/storage/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/theme/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'dolotgia_tot');
+define('DB_PASSWORD', 'b]7I@k6padKD');
+define('DB_DATABASE', 'dolotgia_tot');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
